b9dae27fd86d6ac4657eb5994af9fb446be4e842
karlosos <karlososhd@gmail.com>
2014-08-21 21:20:50 +0200

    Zapis do xml dla excela
    Zmieniam format xml, wazy mniej

--- Praktyki/Parser/source/saving.h
+++ Praktyki/Parser/source/saving.h
@@ -44,8 +44,8 @@ void save_to_xml(string word, string* type, string* scope, vector<string>* commo
 		string xml_type = "\t\t\t<type>" + *type + "</type> \n";
 
 		string xml_scope = "\t\t\t<scope>" + *scope + "</scope> \n";
-		string xml_synonyms_start = "\t\t\t<synonyms> \n";
-		string xml_synonyms_end = "\t\t\t</synonyms> \n";
+		string xml_synonyms_start = "\t\t\t<synonyms>";
+		string xml_synonyms_end = "</synonyms> \n";
 		string xml_synonym = "";
 
 		// Zapisywanie kodu xml
@@ -56,7 +56,9 @@ void save_to_xml(string word, string* type, string* scope, vector<string>* commo
 			out_xml << xml_scope;
 			out_xml << xml_synonyms_start;
 			for (int i=0; i<common_syn_weight; i++) {
-				xml_synonym = "\t\t\t\t<synonym>" +  common_syn->at(i) + "</synonym> \n";
+				if (i>0 && i<common_syn_weight-1) 
+					out_xml << ", ";
+				xml_synonym = common_syn->at(i);
 				out_xml << xml_synonym;
 				xml_synonym = "";
 			}
@@ -71,7 +73,9 @@ void save_to_xml(string word, string* type, string* scope, vector<string>* commo
 			out_xml << xml_scope;
 			out_xml << xml_synonyms_start;
 			for (int i=0; i<syn_weight; i++) {
-				xml_synonym = "\t\t\t\t<synonym>" +  syn->at(i) + "</synonym> \n";
+				if (i>0 && i<syn_weight-1) 
+					out_xml << ", ";
+				xml_synonym = syn->at(i);
 				out_xml << xml_synonym;
 				xml_synonym = "";
 			}
@@ -106,8 +110,8 @@ void save_to_xml(string word, vector<string>* syn) {
 		//string xml_weight = "\t\t\t<weight> \n" + + "</weight> \n";
 
 		string xml_scope = "\t\t\t<scope>" + word + "</scope> \n";
-		string xml_synonyms_start = "\t\t\t<synonyms> \n";
-		string xml_synonyms_end = "\t\t\t</synonyms> \n";
+		string xml_synonyms_start = "\t\t\t<synonyms>";
+		string xml_synonyms_end = "</synonyms> \n";
 		string xml_synonym = "";
 
 		if (syn_weight > 0) {
@@ -117,7 +121,9 @@ void save_to_xml(string word, vector<string>* syn) {
 			out_xml << xml_scope;
 			out_xml << xml_synonyms_start;
 			for (int i=0; i<syn_weight; i++) {
-				xml_synonym = "\t\t\t\t<synonym>" +  syn->at(i) + "</synonym> \n";
+				if (i>0 && i<syn_weight-1) 
+					out_xml << ", ";
+				xml_synonym = syn->at(i);
 				out_xml << xml_synonym;
 				xml_synonym = "";
 			}
@@ -127,6 +133,119 @@ void save_to_xml(string word, vector<string>* syn) {
 
 		out_xml.close();
 }
+
+/////
+///// Zapisuje dane do xml
+/////
+///// @string word - nazwa slowa
+///// @string* type - wskaznik na typ
+///// @string* scope - wskaznik na scope
+///// @vector<string>* common_syn - wskaznik na kontener common synonimow
+///// @vector<string>* syn - wskaznik na kontener pozostalych synonimow
+/////
+//void save_to_xml(string word, string* type, string* scope, vector<string>* common_syn, vector<string>* syn, string file_name) {
+//		// Output plik index.html, otwieramy w formie dopisywania
+//		std::ofstream out_xml;
+//		out_xml.open("zadanie2.xml", std::ios::app);
+//
+//		long double syn_weight = syn->size();
+//		long double common_syn_weight = common_syn->size();
+//
+//		// Lancuchy xml
+//		string xml_word_start = "\t\t<word> \n";
+//		string xml_word_end = "\t\t</word> \n";
+//		string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
+//
+//		//string xml_syn_weight = to_string(syn_weight);
+//		//string xml_common_syn_weight = to_string(common_syn_weight);
+//
+//		//string xml_weight = "\t\t\t<weight> \n" + + "</weight> \n";
+//		string xml_type = "\t\t\t<type>" + *type + "</type> \n";
+//
+//		string xml_scope = "\t\t\t<scope>" + *scope + "</scope> \n";
+//		string xml_synonyms_start = "\t\t\t<synonyms> \n";
+//		string xml_synonyms_end = "\t\t\t</synonyms> \n";
+//		string xml_synonym = "";
+//
+//		// Zapisywanie kodu xml
+//		if (common_syn_weight > 0) {
+//			out_xml << xml_word_start;
+//			out_xml << xml_keyword;
+//			out_xml << "\t\t\t<weight>" + to_string(common_syn_weight) + "</weight> \n";
+//			out_xml << xml_scope;
+//			out_xml << xml_synonyms_start;
+//			for (int i=0; i<common_syn_weight; i++) {
+//				xml_synonym = "\t\t\t\t<synonym>" +  common_syn->at(i) + "</synonym> \n";
+//				out_xml << xml_synonym;
+//				xml_synonym = "";
+//			}
+//			out_xml << xml_synonyms_end;
+//			out_xml << xml_word_end;
+//		}
+//
+//		if (syn_weight > 0) {
+//			out_xml << xml_word_start;
+//			out_xml << xml_keyword;
+//			out_xml << "\t\t\t<weight>" + to_string(syn_weight) + "</weight> \n";
+//			out_xml << xml_scope;
+//			out_xml << xml_synonyms_start;
+//			for (int i=0; i<syn_weight; i++) {
+//				xml_synonym = "\t\t\t\t<synonym>" +  syn->at(i) + "</synonym> \n";
+//				out_xml << xml_synonym;
+//				xml_synonym = "";
+//			}
+//			out_xml << xml_synonyms_end;
+//			out_xml << xml_word_end;
+//		}
+//
+//		out_xml.close();
+//}
+//
+/////
+///// Zapisuje dane do xml
+/////
+///// @string word - nazwa slowa
+///// @vector<string>* syn - wskaznik na kontener pozostalych synonimow
+/////
+//void save_to_xml(string word, vector<string>* syn) {
+//		// Output plik index.html, otwieramy w formie dopisywania
+//		std::ofstream out_xml;
+//		out_xml.open("zadanie1.xml", std::ios::app);
+//
+//		long double syn_weight = syn->size();
+//
+//		// Lancuchy xml
+//		string xml_word_start = "\t\t<word> \n";
+//		string xml_word_end = "\t\t</word> \n";
+//		string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
+//
+//		//string xml_syn_weight = to_string(syn_weight);
+//		//string xml_common_syn_weight = to_string(common_syn_weight);
+//
+//		//string xml_weight = "\t\t\t<weight> \n" + + "</weight> \n";
+//
+//		string xml_scope = "\t\t\t<scope>" + word + "</scope> \n";
+//		string xml_synonyms_start = "\t\t\t<synonyms> \n";
+//		string xml_synonyms_end = "\t\t\t</synonyms> \n";
+//		string xml_synonym = "";
+//
+//		if (syn_weight > 0) {
+//			out_xml << xml_word_start;
+//			out_xml << xml_keyword;
+//			out_xml << "\t\t\t<weight>" + to_string(syn_weight) + "</weight> \n";
+//			out_xml << xml_scope;
+//			out_xml << xml_synonyms_start;
+//			for (int i=0; i<syn_weight; i++) {
+//				xml_synonym = "\t\t\t\t<synonym>" +  syn->at(i) + "</synonym> \n";
+//				out_xml << xml_synonym;
+//				xml_synonym = "";
+//			}
+//			out_xml << xml_synonyms_end;
+//			out_xml << xml_word_end;
+//		}
+//
+//		out_xml.close();
+//}
 
 ///
 /// Zapisuje dane do xml
