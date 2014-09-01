123869112b25cb441b3cbc2848342128f0371e60
karlosos <karlososhd@gmail.com>
2014-09-01 11:54:17 +0200

    Refactoring

--- Praktyki/Parser/source/downloading.h
+++ Praktyki/Parser/source/downloading.h
@@ -8,29 +8,29 @@
 /// @param string word - slowo dla ktorego pobieramy plik
 ///
 void download_wordnik(string word) {
-		string log_word = word;
-		std::wstring w_word(word.begin(), word.end());
-		std::wstring url = std::wstring(TEXT("https://www.wordnik.com/words/")) + w_word + TEXT("/");
-		TCHAR currentpath[MAX_PATH];
-		GetCurrentDirectory(MAX_PATH, currentpath);
-		std::wstring path = std::wstring(currentpath) + TEXT("\\wordnik\\") + w_word + TEXT(".txt");
-		HRESULT res = URLDownloadToFile(NULL, url.c_str(), path.c_str(), 0, NULL);
- 
-        // Checking download
-        if(res == S_OK) {
-                //printf("Ok\n");
-        } else if(res == E_OUTOFMEMORY) {
-                printf("Buffer length invalid, or insufficient memory\n");
-				save_log("Buffer length invalid, or insufficient memory for word: " + log_word, currentDateTime());
-        } else if(res == INET_E_DOWNLOAD_FAILURE) {
-                printf("URL is invalid\n");
-				save_log("URL is invalid for word: " + log_word, currentDateTime());
-        } else {
-                printf("Other error: %d\n", res);
-				save_log("Other error for word: " + log_word, currentDateTime());
-        }
+	string log_word = word;
+	std::wstring w_word(word.begin(), word.end());
+	std::wstring url = std::wstring(TEXT("https://www.wordnik.com/words/")) + w_word + TEXT("/");
+	TCHAR currentpath[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, currentpath);
+	std::wstring path = std::wstring(currentpath) + TEXT("\\wordnik\\") + w_word + TEXT(".txt");
+	HRESULT res = URLDownloadToFile(NULL, url.c_str(), path.c_str(), 0, NULL);
 
-		//cout << "Download wordnik: " << word << endl;
+	// Checking download
+	if(res == S_OK) {
+		//printf("Ok\n");
+	} else if(res == E_OUTOFMEMORY) {
+		printf("Buffer length invalid, or insufficient memory\n");
+		save_log("Buffer length invalid, or insufficient memory for word: " + log_word, currentDateTime());
+	} else if(res == INET_E_DOWNLOAD_FAILURE) {
+		printf("URL is invalid\n");
+		save_log("URL is invalid for word: " + log_word, currentDateTime());
+	} else {
+		printf("Other error: %d\n", res);
+		save_log("Other error for word: " + log_word, currentDateTime());
+	}
+
+	//cout << "Download wordnik: " << word << endl;
 }
 
 ///
@@ -39,57 +39,57 @@ void download_wordnik(string word) {
 /// @param string word - slowo dla ktorego pobieramy plik
 ///
 void download_thesaurus(string word) {
-		string log_word = word;
-		std::wstring w_word(word.begin(), word.end());
-		std::wstring url = std::wstring(TEXT("http://thesaurus.com/browse/")) + w_word + TEXT("/");
-		TCHAR currentpath[MAX_PATH];
-		GetCurrentDirectory(MAX_PATH, currentpath);
-		std::wstring path = std::wstring(currentpath) + TEXT("\\thesaurus\\") + w_word + TEXT(".txt");
-		HRESULT res = URLDownloadToFile(NULL, url.c_str(), path.c_str(), 0, NULL);
- 
-        // Checking download
-        if(res == S_OK) {
-                //printf("Ok\n");
-        } else if(res == E_OUTOFMEMORY) {
-                printf("Buffer length invalid, or insufficient memory\n");
-				save_log("Buffer length invalid, or insufficient memory for word: " + log_word, currentDateTime());
-        } else if(res == INET_E_DOWNLOAD_FAILURE) {
-                printf("URL is invalid\n");
-				save_log("URL is invalid for word: " + log_word, currentDateTime());
-        } else {
-                printf("Other error: %d\n", res);
-				save_log("Other error for word: " + log_word, currentDateTime());
-        }
+	string log_word = word;
+	std::wstring w_word(word.begin(), word.end());
+	std::wstring url = std::wstring(TEXT("http://thesaurus.com/browse/")) + w_word + TEXT("/");
+	TCHAR currentpath[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, currentpath);
+	std::wstring path = std::wstring(currentpath) + TEXT("\\thesaurus\\") + w_word + TEXT(".txt");
+	HRESULT res = URLDownloadToFile(NULL, url.c_str(), path.c_str(), 0, NULL);
 
-		//cout << "Download wordnik: " << word << endl;
+	// Checking download
+	if(res == S_OK) {
+		//printf("Ok\n");
+	} else if(res == E_OUTOFMEMORY) {
+		printf("Buffer length invalid, or insufficient memory\n");
+		save_log("Buffer length invalid, or insufficient memory for word: " + log_word, currentDateTime());
+	} else if(res == INET_E_DOWNLOAD_FAILURE) {
+		printf("URL is invalid\n");
+		save_log("URL is invalid for word: " + log_word, currentDateTime());
+	} else {
+		printf("Other error: %d\n", res);
+		save_log("Other error for word: " + log_word, currentDateTime());
+	}
+
+	//cout << "Download wordnik: " << word << endl;
 }
- 
+
 ///
 /// pobiera plik z wordnetcode
 ///
 void download_wordnetcode() {
 	string log_word = "wordnetcode";
-    TCHAR url[] = TEXT("http://wordnetcode.princeton.edu/standoff-files/core-wordnet.txt");
-    printf("Url: %S\n", url);
-    TCHAR path[MAX_PATH];
-    GetCurrentDirectory(MAX_PATH, path);
-    wsprintf(path, TEXT("%s\\wordnetcode.txt"), path);
-    printf("Path: %S\n", path);
-    HRESULT res = URLDownloadToFile(NULL, url, path, 0, NULL);
- 
-        // Checking download
-        if(res == S_OK) {
-                //printf("Ok\n");
-        } else if(res == E_OUTOFMEMORY) {
-                printf("Buffer length invalid, or insufficient memory\n");
-				save_log("Buffer length invalid, or insufficient memory for file: " + log_word, currentDateTime());
-        } else if(res == INET_E_DOWNLOAD_FAILURE) {
-                printf("URL is invalid\n");
-				save_log("URL is invalid for file: " + log_word, currentDateTime());
-        } else {
-                printf("Other error: %d\n", res);
-				save_log("Other error for file: " + log_word, currentDateTime());
-        }
+	TCHAR url[] = TEXT("http://wordnetcode.princeton.edu/standoff-files/core-wordnet.txt");
+	printf("Url: %S\n", url);
+	TCHAR path[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, path);
+	wsprintf(path, TEXT("%s\\wordnetcode.txt"), path);
+	printf("Path: %S\n", path);
+	HRESULT res = URLDownloadToFile(NULL, url, path, 0, NULL);
+
+	// Checking download
+	if(res == S_OK) {
+		//printf("Ok\n");
+	} else if(res == E_OUTOFMEMORY) {
+		printf("Buffer length invalid, or insufficient memory\n");
+		save_log("Buffer length invalid, or insufficient memory for file: " + log_word, currentDateTime());
+	} else if(res == INET_E_DOWNLOAD_FAILURE) {
+		printf("URL is invalid\n");
+		save_log("URL is invalid for file: " + log_word, currentDateTime());
+	} else {
+		printf("Other error: %d\n", res);
+		save_log("Other error for file: " + log_word, currentDateTime());
+	}
 }
 
 ///
@@ -97,27 +97,27 @@ void download_wordnetcode() {
 ///
 void download_wiktionary() {   
 	string log_word = "wiktionary";
-    TCHAR url[] = TEXT("http://en.wiktionary.org/wiki/Wiktionary:All_Wikisaurus_pages");
-    printf("Url: %S\n", url);
-    TCHAR path[MAX_PATH];
-    GetCurrentDirectory(MAX_PATH, path);
-    wsprintf(path, TEXT("%s\\wiktionary.txt"), path);
-    printf("Path: %S\n", path);
-    HRESULT res = URLDownloadToFile(NULL, url, path, 0, NULL);
- 
-        // Checking download
-        if(res == S_OK) {
-                //printf("Ok\n");
-        } else if(res == E_OUTOFMEMORY) {
-                printf("Buffer length invalid, or insufficient memory\n");
-				save_log("Buffer length invalid, or insufficient memory for file: " + log_word, currentDateTime());
-        } else if(res == INET_E_DOWNLOAD_FAILURE) {
-                printf("URL is invalid\n");
-				save_log("URL is invalid for file: " + log_word, currentDateTime());
-        } else {
-                printf("Other error: %d\n", res);
-				save_log("Other error for file: " + log_word, currentDateTime());
-        }
+	TCHAR url[] = TEXT("http://en.wiktionary.org/wiki/Wiktionary:All_Wikisaurus_pages");
+	printf("Url: %S\n", url);
+	TCHAR path[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, path);
+	wsprintf(path, TEXT("%s\\wiktionary.txt"), path);
+	printf("Path: %S\n", path);
+	HRESULT res = URLDownloadToFile(NULL, url, path, 0, NULL);
+
+	// Checking download
+	if(res == S_OK) {
+		//printf("Ok\n");
+	} else if(res == E_OUTOFMEMORY) {
+		printf("Buffer length invalid, or insufficient memory\n");
+		save_log("Buffer length invalid, or insufficient memory for file: " + log_word, currentDateTime());
+	} else if(res == INET_E_DOWNLOAD_FAILURE) {
+		printf("URL is invalid\n");
+		save_log("URL is invalid for file: " + log_word, currentDateTime());
+	} else {
+		printf("Other error: %d\n", res);
+		save_log("Other error for file: " + log_word, currentDateTime());
+	}
 
 }
 #endif //DOWNLOADING_H
--- Praktyki/Parser/source/saving.h
+++ Praktyki/Parser/source/saving.h
@@ -51,8 +51,8 @@ void scope_sort() {
 		}
 	}
 
-	  sort(scope_words.begin(), scope_words.end());
-      scope_words.erase(unique(scope_words.begin(), scope_words.end() ), scope_words.end() );
+	sort(scope_words.begin(), scope_words.end());
+	scope_words.erase(unique(scope_words.begin(), scope_words.end() ), scope_words.end() );
 }
 
 ///
@@ -64,7 +64,7 @@ void scope_add(string scope, string* type) {
 	scope_words.clear();
 	scope_words_tmp.clear();
 	scope_type = *type;
-    std::size_t prev_pos = 0, pos;
+	std::size_t prev_pos = 0, pos;
 
 	size_t comma = scope.find(",");
 	size_t semicolon = scope.find(";");
@@ -91,11 +91,11 @@ void scope_add(string scope, string* type) {
 ///@param string date - data
 ///
 void save_log(string message, string date) {
-		// Output plik index.html, otwieramy w formie dopisywania
-		std::ofstream out;
-		out.open(log_name, std::ios::app);
-		out << message + " at " + date + " \n";
-		out.close();
+	// Output plik index.html, otwieramy w formie dopisywania
+	std::ofstream out;
+	out.open(log_name, std::ios::app);
+	out << message + " at " + date + " \n";
+	out.close();
 }
 
 ///
@@ -108,63 +108,63 @@ void save_log(string message, string date) {
 /// @vector<string>* syn - wskaznik na kontener pozostalych synonimow
 ///
 void save_to_xml(string word, string scope, string* type, vector<string>* common_syn, vector<string>* syn, string file_name) {
-		// Output plik index.html, otwieramy w formie dopisywania
-		std::ofstream out_xml;
-		out_xml.open("zadanie2.xml", std::ios::app);
+	// Output plik index.html, otwieramy w formie dopisywania
+	std::ofstream out_xml;
+	out_xml.open("zadanie2.xml", std::ios::app);
 
-		long double syn_weight = syn->size();
-		long double common_syn_weight = common_syn->size();
+	long double syn_weight = syn->size();
+	long double common_syn_weight = common_syn->size();
 
-		// Lancuchy xml
-		string xml_word_start = "\t\t<word> \n";
-		string xml_word_end = "\t\t</word> \n";
-		string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
+	// Lancuchy xml
+	string xml_word_start = "\t\t<word> \n";
+	string xml_word_end = "\t\t</word> \n";
+	string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
 
-		string xml_type = "\t\t\t<type>" + *type + "</type> \n";
+	string xml_type = "\t\t\t<type>" + *type + "</type> \n";
 
-		string xml_scope = "\t\t\t<scope>" + scope + "</scope> \n";
-		string xml_synonyms_start = "\t\t\t<synonyms>";
-		string xml_synonyms_end = "</synonyms> \n";
-		string xml_synonym = "";
+	string xml_scope = "\t\t\t<scope>" + scope + "</scope> \n";
+	string xml_synonyms_start = "\t\t\t<synonyms>";
+	string xml_synonyms_end = "</synonyms> \n";
+	string xml_synonym = "";
 
-		// Zapisywanie kodu xml
-		if (common_syn_weight > 0) {
-			out_xml << xml_word_start;
-			out_xml << xml_keyword;
-			out_xml << "\t\t\t<weight>80</weight> \n";
-			out_xml << xml_type;
-			out_xml << xml_scope;
-			out_xml << xml_synonyms_start;
-			for (int i=0; i<common_syn_weight; i++) {
-				if (i>0 && i<common_syn_weight) 
-					out_xml << ", ";
-				xml_synonym = common_syn->at(i);
-				out_xml << xml_synonym;
-				xml_synonym = "";
-			}
-			out_xml << xml_synonyms_end;
-			out_xml << xml_word_end;
+	// Zapisywanie kodu xml
+	if (common_syn_weight > 0) {
+		out_xml << xml_word_start;
+		out_xml << xml_keyword;
+		out_xml << "\t\t\t<weight>80</weight> \n";
+		out_xml << xml_type;
+		out_xml << xml_scope;
+		out_xml << xml_synonyms_start;
+		for (int i=0; i<common_syn_weight; i++) {
+			if (i>0 && i<common_syn_weight) 
+				out_xml << ", ";
+			xml_synonym = common_syn->at(i);
+			out_xml << xml_synonym;
+			xml_synonym = "";
 		}
+		out_xml << xml_synonyms_end;
+		out_xml << xml_word_end;
+	}
 
-		if (syn_weight > 0) {
-			out_xml << xml_word_start;
-			out_xml << xml_keyword;
-			out_xml << "\t\t\t<weight>40</weight> \n";
-			out_xml << xml_type;
-			out_xml << xml_scope;
-			out_xml << xml_synonyms_start;
-			for (int i=0; i<syn_weight; i++) {
-				if (i>0 && i<syn_weight) 
-					out_xml << ", ";
-				xml_synonym = syn->at(i);
-				out_xml << xml_synonym;
-				xml_synonym = "";
-			}
-			out_xml << xml_synonyms_end;
-			out_xml << xml_word_end;
+	if (syn_weight > 0) {
+		out_xml << xml_word_start;
+		out_xml << xml_keyword;
+		out_xml << "\t\t\t<weight>40</weight> \n";
+		out_xml << xml_type;
+		out_xml << xml_scope;
+		out_xml << xml_synonyms_start;
+		for (int i=0; i<syn_weight; i++) {
+			if (i>0 && i<syn_weight) 
+				out_xml << ", ";
+			xml_synonym = syn->at(i);
+			out_xml << xml_synonym;
+			xml_synonym = "";
 		}
+		out_xml << xml_synonyms_end;
+		out_xml << xml_word_end;
+	}
 
-		out_xml.close();
+	out_xml.close();
 }
 
 ///
@@ -174,41 +174,41 @@ void save_to_xml(string word, string scope, string* type, vector<string>* common
 /// @vector<string>* syn - wskaznik na kontener pozostalych synonimow
 ///
 void save_to_xml(string word, vector<string>* syn, int weight) {
-		// Output plik index.html, otwieramy w formie dopisywania
-		std::ofstream out_xml;
-		out_xml.open("zadanie1.xml", std::ios::app);
+	// Output plik index.html, otwieramy w formie dopisywania
+	std::ofstream out_xml;
+	out_xml.open("zadanie1.xml", std::ios::app);
 
-		long double syn_weight = syn->size();
-		long double w_weight = weight;
+	long double syn_weight = syn->size();
+	long double w_weight = weight;
 
-		// Lancuchy xml
-		string xml_word_start = "\t\t<word> \n";
-		string xml_word_end = "\t\t</word> \n";
-		string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
+	// Lancuchy xml
+	string xml_word_start = "\t\t<word> \n";
+	string xml_word_end = "\t\t</word> \n";
+	string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
 
-		string xml_scope = "\t\t\t<scope>" + word + "</scope> \n";
-		string xml_synonyms_start = "\t\t\t<synonyms>";
-		string xml_synonyms_end = "</synonyms> \n";
-		string xml_synonym = "";
+	string xml_scope = "\t\t\t<scope>" + word + "</scope> \n";
+	string xml_synonyms_start = "\t\t\t<synonyms>";
+	string xml_synonyms_end = "</synonyms> \n";
+	string xml_synonym = "";
 
-		if (syn_weight > 0) {
-			out_xml << xml_word_start;
-			out_xml << xml_keyword;
-			out_xml << "\t\t\t<weight>" + to_string(w_weight) + "</weight> \n";
-			out_xml << xml_scope;
-			out_xml << xml_synonyms_start;
-			for (int i=0; i<syn_weight; i++) {
-				if (i>0 && i<syn_weight) 
-					out_xml << ", ";
-				xml_synonym = syn->at(i);
-				out_xml << xml_synonym;
-				xml_synonym = "";
-			}
-			out_xml << xml_synonyms_end;
-			out_xml << xml_word_end;
+	if (syn_weight > 0) {
+		out_xml << xml_word_start;
+		out_xml << xml_keyword;
+		out_xml << "\t\t\t<weight>" + to_string(w_weight) + "</weight> \n";
+		out_xml << xml_scope;
+		out_xml << xml_synonyms_start;
+		for (int i=0; i<syn_weight; i++) {
+			if (i>0 && i<syn_weight) 
+				out_xml << ", ";
+			xml_synonym = syn->at(i);
+			out_xml << xml_synonym;
+			xml_synonym = "";
 		}
+		out_xml << xml_synonyms_end;
+		out_xml << xml_word_end;
+	}
 
-		out_xml.close();
+	out_xml.close();
 }
 
 ///
@@ -221,77 +221,77 @@ void save_to_xml(string word, vector<string>* syn, int weight) {
 /// @vector<string>* syn - wskaznik na kontener pozostalych synonimow
 ///
 void save_to_html(string word, string scope, string* type,vector<string>* common_syn, vector<string>* syn, string file_name, int deep){
-	
-		// Output plik index.html, otwieramy w formie dopisywania
-		std::ofstream out_html;
-		out_html.open("zadanie2.html", std::ios::app);
 
-		long double syn_weight = syn->size();
-		long double common_syn_weight = common_syn->size();
+	// Output plik index.html, otwieramy w formie dopisywania
+	std::ofstream out_html;
+	out_html.open("zadanie2.html", std::ios::app);
 
-		// Lancuchy xml
-		string html_word_start = "\t <tr id=\"word\"> \n";
-		if (deep == 1)
-			html_word_start = "\t <tr id=\"word\" class=\"deep\"> \n";
-		string html_word_end = "\t </tr> \n";
-		string html_keyword = "\t\t<td id=\"keyword\">" + word + "</td> \n";
+	long double syn_weight = syn->size();
+	long double common_syn_weight = common_syn->size();
 
-		string html_type = "\t \t <td id=\"type\">" + *type + "</td> \n";
+	// Lancuchy xml
+	string html_word_start = "\t <tr id=\"word\"> \n";
+	if (deep == 1)
+		html_word_start = "\t <tr id=\"word\" class=\"deep\"> \n";
+	string html_word_end = "\t </tr> \n";
+	string html_keyword = "\t\t<td id=\"keyword\">" + word + "</td> \n";
 
-		string html_scope = "\t \t <td id=\"scope\">" + scope + "</td> \n";
-		string html_synonyms_start = "\t \t <td id=\"syn\">";
-		string html_synonyms_end = "</td> \n";
-		string html_synonym = "";
+	string html_type = "\t \t <td id=\"type\">" + *type + "</td> \n";
 
-		// Zapisywanie kodu xml
-		if (common_syn_weight > 0) {
-			out_html << html_word_start;
-			out_html << html_keyword;
-			out_html << "\t \t <td id=\"weight\">80</td> \n";
-			out_html << html_type;
-			out_html << html_scope;
-			out_html << html_synonyms_start;
-			for (int i=0; i<common_syn_weight; i++) {
-				if (i>0) 
-					out_html << ", ";
-				html_synonym = common_syn->at(i);
-				out_html << html_synonym;
-				html_synonym = "";
-			}
-			out_html << html_synonyms_end;
-			out_html << html_word_end;
+	string html_scope = "\t \t <td id=\"scope\">" + scope + "</td> \n";
+	string html_synonyms_start = "\t \t <td id=\"syn\">";
+	string html_synonyms_end = "</td> \n";
+	string html_synonym = "";
+
+	// Zapisywanie kodu xml
+	if (common_syn_weight > 0) {
+		out_html << html_word_start;
+		out_html << html_keyword;
+		out_html << "\t \t <td id=\"weight\">80</td> \n";
+		out_html << html_type;
+		out_html << html_scope;
+		out_html << html_synonyms_start;
+		for (int i=0; i<common_syn_weight; i++) {
+			if (i>0) 
+				out_html << ", ";
+			html_synonym = common_syn->at(i);
+			out_html << html_synonym;
+			html_synonym = "";
 		}
-
-		if (syn_weight > 0) {
-			out_html << html_word_start;
-			out_html << html_keyword;
-			out_html << "\t \t <td id=\"weight\">40</td> \n";
-			out_html << html_type;
-			out_html << html_scope;
-			out_html << html_synonyms_start;
-			for (int i=0; i<syn_weight; i++) {
-				if (i>0) 
-					out_html << ", ";
-				html_synonym = syn->at(i);
-				out_html << html_synonym;
-				html_synonym = "";
-			}
-			out_html << html_synonyms_end;
-			out_html << html_word_end;
-		}
-		out_html.close();
-
-		if (previous_scope != scope) {
-			previous_scope = scope;
-			scope_add(scope, type);
-			scope_clean();
-			scope_sort();
-			scope_save(word);
-			scope_words.clear();
-			scope_words_tmp.clear();
-			scope_type = "";
-		}
+		out_html << html_synonyms_end;
+		out_html << html_word_end;
 	}
+
+	if (syn_weight > 0) {
+		out_html << html_word_start;
+		out_html << html_keyword;
+		out_html << "\t \t <td id=\"weight\">40</td> \n";
+		out_html << html_type;
+		out_html << html_scope;
+		out_html << html_synonyms_start;
+		for (int i=0; i<syn_weight; i++) {
+			if (i>0) 
+				out_html << ", ";
+			html_synonym = syn->at(i);
+			out_html << html_synonym;
+			html_synonym = "";
+		}
+		out_html << html_synonyms_end;
+		out_html << html_word_end;
+	}
+	out_html.close();
+
+	if (previous_scope != scope) {
+		previous_scope = scope;
+		scope_add(scope, type);
+		scope_clean();
+		scope_sort();
+		scope_save(word);
+		scope_words.clear();
+		scope_words_tmp.clear();
+		scope_type = "";
+	}
+}
 
 ///
 /// Zapisuje dane do xml
@@ -301,39 +301,39 @@ void save_to_html(string word, string scope, string* type,vector<string>* common
 ///
 void save_to_html(string word, vector<string>* syn, int weight){
 	// Output plik index.html, otwieramy w formie dopisywania
-		std::ofstream out_html;
-		out_html.open("zadanie1.html", std::ios::app);
+	std::ofstream out_html;
+	out_html.open("zadanie1.html", std::ios::app);
 
-		long double syn_weight = syn->size();
-		long double w_weight = weight;
-		// Lancuchy xml
-		string html_word_start = "\t <tr id=\"word\"> \n";
-		string html_word_end = "\t </tr> \n";
-		string html_keyword = "\t\t<td id=\"keyword\">" + word + "</td> \n";
+	long double syn_weight = syn->size();
+	long double w_weight = weight;
+	// Lancuchy xml
+	string html_word_start = "\t <tr id=\"word\"> \n";
+	string html_word_end = "\t </tr> \n";
+	string html_keyword = "\t\t<td id=\"keyword\">" + word + "</td> \n";
 
-		string html_scope = "\t \t <td id=\"scope\">" + word + "</td> \n";
-		string html_synonyms_start = "\t \t <td id=\"syn\">";
-		string html_synonyms_end = "</td> \n";
-		string html_synonym = "";
+	string html_scope = "\t \t <td id=\"scope\">" + word + "</td> \n";
+	string html_synonyms_start = "\t \t <td id=\"syn\">";
+	string html_synonyms_end = "</td> \n";
+	string html_synonym = "";
 
-		if (syn_weight > 0) {
-			out_html << html_word_start;
-			out_html << html_keyword;
-			out_html << "\t \t <td id=\"weight\">" + to_string(w_weight) + "</td> \n";
-			out_html << html_scope;
-			out_html << html_synonyms_start;
-			for (int i=0; i<syn_weight; i++) {
-				if (i>0) 
-					out_html << ", ";
-				html_synonym = syn->at(i);
-				out_html << html_synonym;
-				html_synonym = "";
-			}
-			out_html << html_synonyms_end;
-			out_html << html_word_end;
+	if (syn_weight > 0) {
+		out_html << html_word_start;
+		out_html << html_keyword;
+		out_html << "\t \t <td id=\"weight\">" + to_string(w_weight) + "</td> \n";
+		out_html << html_scope;
+		out_html << html_synonyms_start;
+		for (int i=0; i<syn_weight; i++) {
+			if (i>0) 
+				out_html << ", ";
+			html_synonym = syn->at(i);
+			out_html << html_synonym;
+			html_synonym = "";
 		}
+		out_html << html_synonyms_end;
+		out_html << html_word_end;
+	}
 
-		out_html.close();
+	out_html.close();
 }
 
 ///
@@ -376,89 +376,89 @@ void scope_save_html(string word, string scope) {
 /// @param string scope
 ///
 void scope_save_xml(string word, string scope) {
-		// Output plik index.html, otwieramy w formie dopisywania
-		std::ofstream out_xml;
-		out_xml.open("zadanie2.xml", std::ios::app);
+	// Output plik index.html, otwieramy w formie dopisywania
+	std::ofstream out_xml;
+	out_xml.open("zadanie2.xml", std::ios::app);
 
-		// Lancuchy xml
-		string xml_word_start = "\t\t<word> \n";
-		string xml_word_end = "\t\t</word> \n";
-		string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
+	// Lancuchy xml
+	string xml_word_start = "\t\t<word> \n";
+	string xml_word_end = "\t\t</word> \n";
+	string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
 
-		string xml_type = "\t\t\t<type>"+ scope_type +"</type> \n";
+	string xml_type = "\t\t\t<type>"+ scope_type +"</type> \n";
 
-		string xml_scope = "\t\t\t<scope>" + scope + "</scope> \n";
-		string xml_synonyms = "\t\t\t<synonyms>" + scope + "</synonyms> \n";
+	string xml_scope = "\t\t\t<scope>" + scope + "</scope> \n";
+	string xml_synonyms = "\t\t\t<synonyms>" + scope + "</synonyms> \n";
 
-		// Zapisywanie kodu xml
-		out_xml << xml_word_start;
-		out_xml << xml_keyword;
-		out_xml << "\t\t\t<weight>100</weight> \n";
-		out_xml << xml_type;
-		out_xml << xml_scope;
-		out_xml << xml_synonyms;
-		out_xml << xml_word_end;
+	// Zapisywanie kodu xml
+	out_xml << xml_word_start;
+	out_xml << xml_keyword;
+	out_xml << "\t\t\t<weight>100</weight> \n";
+	out_xml << xml_type;
+	out_xml << xml_scope;
+	out_xml << xml_synonyms;
+	out_xml << xml_word_end;
 
-		out_xml.close();
+	out_xml.close();
 }
 
 void scope_save_corrupt_xml(string word, string scope) {
-		cout << "Dodano wiersz corrupt. Word: " + word + " scope: " + scope + currentDateTime() << endl;
-		save_log("Dodano wiersz corrupt. Word: " + word + " scope: " + scope, currentDateTime());
+	cout << "Dodano wiersz corrupt. Word: " + word + " scope: " + scope + currentDateTime() << endl;
+	save_log("Dodano wiersz corrupt. Word: " + word + " scope: " + scope, currentDateTime());
 
-		// Output plik index.html, otwieramy w formie dopisywania
-		std::ofstream out_xml;
-		out_xml.open("zadanie2_corrupt.xml", std::ios::app);
+	// Output plik index.html, otwieramy w formie dopisywania
+	std::ofstream out_xml;
+	out_xml.open("zadanie2_corrupt.xml", std::ios::app);
 
-		// Lancuchy xml
-		string xml_word_start = "\t\t<word> \n";
-		string xml_word_end = "\t\t</word> \n";
-		string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
+	// Lancuchy xml
+	string xml_word_start = "\t\t<word> \n";
+	string xml_word_end = "\t\t</word> \n";
+	string xml_keyword = "\t\t\t<keyword>" + word + "</keyword> \n";
 
-		string xml_type = "\t\t\t<type>"+ scope_type +"</type> \n";
+	string xml_type = "\t\t\t<type>"+ scope_type +"</type> \n";
 
-		string xml_scope = "\t\t\t<scope>" + scope + "</scope> \n";
-		string xml_synonyms = "\t\t\t<synonyms>" + scope + "</synonyms> \n";
+	string xml_scope = "\t\t\t<scope>" + scope + "</scope> \n";
+	string xml_synonyms = "\t\t\t<synonyms>" + scope + "</synonyms> \n";
 
-		// Zapisywanie kodu xml
-		out_xml << xml_word_start;
-		out_xml << xml_keyword;
-		out_xml << "\t\t\t<weight>100</weight> \n";
-		out_xml << xml_type;
-		out_xml << xml_scope;
-		out_xml << xml_synonyms;
-		out_xml << xml_word_end;
+	// Zapisywanie kodu xml
+	out_xml << xml_word_start;
+	out_xml << xml_keyword;
+	out_xml << "\t\t\t<weight>100</weight> \n";
+	out_xml << xml_type;
+	out_xml << xml_scope;
+	out_xml << xml_synonyms;
+	out_xml << xml_word_end;
 
-		out_xml.close();
+	out_xml.close();
 }
 
 bool fileNotExists(string word, string folder) {
-		// Input file
-		string i_path = folder + "\\" + word + ".txt";
-        ifstream i_file (i_path);
+	// Input file
+	string i_path = folder + "\\" + word + ".txt";
+	ifstream i_file (i_path);
 
-		// Trimming second file
-        if (i_file.is_open()) { 
-			return false;
-		} else {
-			cout << "There's no file. We will download: " << word << endl;
-			save_log("There's no file. We will download: " + word, currentDateTime());
-			return true;
-		}
+	// Trimming second file
+	if (i_file.is_open()) { 
+		return false;
+	} else {
+		cout << "There's no file. We will download: " << word << endl;
+		save_log("There's no file. We will download: " + word, currentDateTime());
+		return true;
+	}
 }
 
 bool fileNotExists(string word) {
-		// Input file
-		string i_path = word + ".txt";
-        ifstream i_file (i_path);
+	// Input file
+	string i_path = word + ".txt";
+	ifstream i_file (i_path);
 
-		// Trimming second file
-        if (i_file.is_open()) { 
-			return false;
-		} else {
-			cout << "There's no file. We will download: " << word << endl;
-			save_log("There's no file. We will download: " + word, currentDateTime());
-			return true;
-		}
+	// Trimming second file
+	if (i_file.is_open()) { 
+		return false;
+	} else {
+		cout << "There's no file. We will download: " << word << endl;
+		save_log("There's no file. We will download: " + word, currentDateTime());
+		return true;
+	}
 }
 #endif //SAVING_H
