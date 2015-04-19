adb5207049bc7864e0859a12234ffb40db7a8dd3
Karol Działowski <karlososhd@gmail.com>
2015-04-19 21:58:32 +0200

    Draw

--- Informatyka/AndroidStudioProjects/SprA/app/src/main/java/com/example/karol/spra/Draw.java
+++ Informatyka/AndroidStudioProjects/SprA/app/src/main/java/com/example/karol/spra/Draw.java
@@ -4,6 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 
 public class Draw extends ActionBarActivity {
@@ -35,5 +40,34 @@ public class Draw extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void draw (View v) {
+        RadioButton square = (RadioButton) findViewById(R.id.square);
+        RadioButton circle = (RadioButton) findViewById(R.id.circle);
+        CheckBox shadow = (CheckBox) findViewById(R.id.shadow);
+        EditText weight = (EditText) findViewById(R.id.weight);
+        String start = "Rysuje ";
+        String shape = "";
+        String mid = " o grubosci ";
+        String px = " px ";
+        String sh = "";
+        String end = " cieniowania";
+        if (square.isChecked()) {
+            shape = "kwadrat";
+        } else if (circle.isChecked()) {
+            shape = "okrag";
+        }
+
+        if (shadow.isChecked()) {
+            sh = "z";
+        } else {
+            sh = "bez";
+        }
+
+        sh = weight.getText().toString();
+
+        Toast toast = Toast.makeText(getApplicationContext(), start + shape + mid + sh + px + end, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
