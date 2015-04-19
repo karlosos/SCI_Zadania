e1c7017a855bc8ae820b82a96d56c4bd9f53caa3
Karol Działowski <karlososhd@gmail.com>
2015-04-19 21:46:01 +0200

    Caller + manifest

--- Informatyka/AndroidStudioProjects/SprA/app/src/main/java/com/example/karol/spra/MainActivity.java
+++ Informatyka/AndroidStudioProjects/SprA/app/src/main/java/com/example/karol/spra/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.karol.spra;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -36,8 +38,10 @@ public class MainActivity extends ActionBarActivity {
             Intent intent  = new Intent(this, Gallery.class);
             startActivity(intent);
         } else if (id == R.id.dialer) {
-            Intent intent  = new Intent(this, Caller.class);
-            startActivity(intent);
+            EditText number = (EditText) findViewById(R.id.editText);
+            Uri call = Uri.parse("tel:" + number);
+            Intent dialer = new Intent(Intent.ACTION_CALL, call);
+            startActivity(dialer);
         } else if (id == R.id.draw) {
             Intent intent  = new Intent(this, Draw.class);
             startActivity(intent);
