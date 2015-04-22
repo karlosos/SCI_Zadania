488047999011dc81a08017309e87b8da9bfbda5a
Karol Działowski <karlososhd@gmail.com>
2015-04-22 18:05:02 +0200

    Spr And

--- Informatyka/AndroidStudioProjects/SprB/app/src/main/java/com/example/karol/sprb/Color.java
+++ Informatyka/AndroidStudioProjects/SprB/app/src/main/java/com/example/karol/sprb/Color.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Color extends ActionBarActivity {
@@ -35,5 +38,16 @@ public class Color extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void color(View v) {
+        EditText r = (EditText)findViewById(R.id.editText);
+        EditText g = (EditText)findViewById(R.id.editText2);
+        EditText b = (EditText)findViewById(R.id.editText3);
+
+        if(r.getText().toString() != "" && g.getText().toString() != "" && b.getText().toString() != "") {
+            Toast toast = Toast.makeText(getApplicationContext(), "RGB("+r.getText().toString()+","+g.getText().toString()+","+b.getText().toString()+")", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
