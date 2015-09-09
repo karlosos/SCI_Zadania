3c65f7e67d97cb945f99acb22fc5e473bbc6239c
Karol Dzialowski <karlososhd@gmail.com>
2015-09-09 12:50:49 +0200

    Zadanie domowe
    Problem z linia 29.
    Nie moglem porownac pierwsza == "S" tylko musialem 'S'

--- Informatyka/matura/main.cpp
+++ Informatyka/matura/main.cpp
@@ -12,21 +12,32 @@ int main(void)
   bool test = file_handle.good();
   std::string napis;
   std::vector<std::string> imiona;
+  std::vector<std::string> imiona_na_s;
   
   while (!file_handle.eof())
     {
       std::getline(file_handle, napis);
       imiona.push_back(napis);
-      //std::cout << napis << std::endl;
+      //std::cout << napis.at(0) << std::endl;
     }
 
   for (int i=0; i<imiona.size(); i++) 
     {
-      std::cout << imiona[i] << std::endl;
+      if(imiona[i].size() > 0)
+      {
+        char pierwsza = imiona[i].at(0);
+        if(pierwsza == 'S') 
+        {
+          imiona_na_s.push_back(imiona[i]);
+          //std::cout << imiona[i] << std::endl;
+        }
+      }
     }
 
-  file_handle.close();
-    return 0;
+  std::cout << imiona_na_s.size();
 
-    //Napisz kod, ktory zapisze do pliku tylko te imiona ktore rozpoczynaja sie na litere S oraz wypisze na konsoli ile takich imion spelnia ten warunek
+  file_handle.close();
+  return 0;
+
+  //Napisz kod, ktory zapisze do pliku tylko te imiona ktore rozpoczynaja sie na litere S oraz wypisze na konsoli ile takich imion spelnia ten warunek
 }
